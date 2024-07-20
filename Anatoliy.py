c1ffdf0770130843fa2bc6b3731c7a2cfe4f5529
Ludmila <lvminzulina@mail.ru>
2024-07-20 16:46:56 +0300

    Task1

---
+++ Anatoliy.py
@@ -0,0 +1,13 @@
+
+list = ['Алиса', 'Андрей', 'Ваня', 'Марина', 'Катя']
+print(len(list))
+
+for i in list:
+    if i.startswith('А'):
+        print(i)
+    if i.endswith('я'):
+        print(i)
+
+
+
+
---
+++ module2hard.py
@@ -0,0 +1,10 @@
+import random
+print(random)
+
+field1 = random.randint(3,20)
+print(field1)
+
+for i in range(1, field1 + 1):
+    n1 = i
+    print(n1)
+    
