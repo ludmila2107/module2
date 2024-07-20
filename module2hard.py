025013496606f736968a309379cd7836a1534280
Ludmila <lvminzulina@mail.ru>
2024-07-20 18:28:12 +0300

    module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,10 +1,17 @@
 import random
+
 print(random)
 
-field1 = random.randint(3,20)
+field1 = random.randint(3, 20)
 print(field1)
-
+result = []
 for i in range(1, field1 + 1):
-    n1 = i
-    print(n1)
-    
+
+    for j in range(i, field1 + 1):
+
+        if field1 % (i + (j + 1)) == 0:
+            result.append(i)
+            result.append(j + 1)
+
+result = ''.join(str(number) for number in result)
+print(result)
